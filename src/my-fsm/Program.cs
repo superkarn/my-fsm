f4bea218717224a88e84d653bccff4a282a6b405
Karn Ratana <karn@ratana.info>
2021-02-13 16:40:02 +0000

    Initial Commit

---
+++ src/my-fsm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace my_fsm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
